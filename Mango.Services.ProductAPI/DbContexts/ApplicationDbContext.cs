9c3d436386b17375ddd80fdda266f01693ee9f49
Redice1997 <roshin-vladimir@mail.ru>
2022-06-22 00:24:22 +0300

    Add Product API project

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base (option)
+    {
+        
+    }
+}
